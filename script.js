2c33ac10ebbd5ce1f764ba825ed349b163c096d5
Nikheel99 <thipparthinikhil@gmail.com>
2023-09-27 11:19:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+function clickOnButton() {
+  const li = document.createElement("li");
+  const inputValue = document.getElementById("input").value;
+  const t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("Please enter your todo");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("input").value = "";
+
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      const div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
+
+const myNodeList = document.getElementsByTagName("LI");
+for (let i = 0; i < myNodeList.length; i++) {
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodeList[i].appendChild(span);
+}
+
+const close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    const div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+const list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
