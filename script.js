1516acbb2fdb0c2edff03a083e89f003027dea41
Nikheel99 <thipparthinikhil@gmail.com>
2023-10-02 21:06:15 +0530

    change in script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function clickOnAddButton() {
   const inputValue = document.getElementById("input").value;
   const t = document.createTextNode(inputValue);
   li.appendChild(t);
-  if (inputValue === "") {
+  if (inputValue === "" || inputValue.trim() === "") {
     alert("Please enter your todo");
   } else {
     document.getElementById("list").appendChild(li);
+    document.getElementById("input").value = "";
   }
-  document.getElementById("input").value = "";
 
   const span = document.createElement("SPAN");
   const txt = document.createTextNode("\u00D7");
@@ -16,29 +16,13 @@ function clickOnAddButton() {
   span.appendChild(txt);
   li.appendChild(span);
 
+  const close = document.getElementsByClassName("close");
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
       const div = this.parentElement;
       div.style.display = "none";
     };
   }
-}
-
-const myNodeList = document.getElementsByTagName("LI");
-for (let i = 0; i < myNodeList.length; i++) {
-  const span = document.createElement("SPAN");
-  const txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodeList[i].appendChild(span);
-}
-
-const close = document.getElementsByClassName("close");
-for (let i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
-    const div = this.parentElement;
-    div.style.display = "none";
-  };
 }
 
 const list = document.querySelector("ul");
